18ccb184e8654001c447026612c533e608e598d0
Ktosox <ktoso3@gmail.com>
2023-08-28 17:44:17 +0200

    Showing path now works according to description

--- Game/Experimental/PathShower.gd
+++ Game/Experimental/PathShower.gd
@@ -6,9 +6,11 @@ extends Navigation2D
 # var b = "text"
 var lockPath = false
 
+var finalPath = Curve2D.new()
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$MoveRangeHiglight.update_dirty_quadrants()
+
 	snap_start()
 	
 	pass # Replace with function body.
@@ -22,11 +24,12 @@ func update_path():
 	$StartingPosition.curve.clear_points()
 
 	#$Path.add_point($StartingPosition.global_position)
-	for P in get_simple_path($StartingPosition.global_position,ceneter_from_global(get_global_mouse_position() - $Path.global_position)):
+	for P in get_simple_path($StartingPosition.global_position,ceneter_from_global(get_global_mouse_position())):
 		$StartingPosition.curve.add_point(P - $StartingPosition.global_position)
 		$Path.add_point(P)
 	#$Path.add_point( ceneter_from_global(get_global_mouse_position() - $Path.global_position))
-	$EndPosition.global_position = ceneter_from_global(get_global_mouse_position())
+	
+	$EndPosition.global_position = $Path.points[$Path.points.size()-1]
 	pass
 
 func snap_start():
@@ -39,18 +42,34 @@ func _process(delta):
 	
 	pass
 
+func add_step():
+	var newStep = $StepMarker.duplicate()
+	add_child_below_node($StepMarker,newStep)
+	for P in get_simple_path($StartingPosition.global_position,ceneter_from_global(get_global_mouse_position())):
+		finalPath.add_point(P)
+	newStep.global_position = $EndPosition.global_position
+	$StartingPosition.global_position = $EndPosition.global_position
+	$FinalPath.points = finalPath.get_baked_points()
+	pass
 
-func _input(event):
-	if event.is_action_pressed("ui_accept"):
-		lockPath = true
+func play_path():
+	lockPath = true
+	$AnimationPlayer.playback_speed = 200 / $StartingPosition.curve.get_baked_length()
+	$AnimationPlayer.play("move")
+	pass
+
+func _input(event:InputEvent):
+	if event.is_pressed() and event.is_class("InputEventMouseButton") :
+		add_step()
+		# add next step
 		
-		$AnimationPlayer.playback_speed = 200 / $StartingPosition.curve.get_baked_length()
-		$AnimationPlayer.play("move")
-		
+		pass
+#
+#		$AnimationPlayer.playback_speed = 200 / $StartingPosition.curve.get_baked_length()
+#
 
 
 func _on_AnimationPlayer_animation_finished(anim_name):
-	lockPath = false
 	$StartingPosition.global_position = $StartingPosition/PathFollow2D.global_position
 	snap_start()
 	$StartingPosition/PathFollow2D.unit_offset = 0
